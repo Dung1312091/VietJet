bf83d58cb65b30cb5714ed3a70cc44921141d4ca
Le QuangDung <quangdung100194@gmail.com>
2018-07-16 18:29:42 +0700

    add some component

--- src/containers/Body/index.js
+++ src/containers/Body/index.js
@@ -4,19 +4,39 @@ import UserProfile from '../../containers/UserProfile';
 import Test from '../../components/Text';
 import DashBoard from '../DashBoard';
 import DashBoardContend from '../../containers/DashBoardContent';
+import Login from '../../components/Login';
 class Body extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isLogin: true
+    };
+  }
   render() {
+    const { isLogin } = this.state;
     return (
       // <Switch>
       //   <Route exact path="/a" component={UserProfile} />
       //   <Route exact path="/" component={DashBoard} />
       // </Switch>
-      <DashBoard>
-        <Switch>
-          <Route exact path="/" component={DashBoardContend} />
-          <Route exact path="/user" component={UserProfile} />
-        </Switch>
-      </DashBoard>
+      // <DashBoard>
+      //   <Switch>
+      //     <Route exact path="/" component={DashBoardContend} />
+      //     <Route exact path="/user" component={UserProfile} />
+      //   </Switch>
+      // </DashBoard>
+      <div>
+        {isLogin ? (
+          <DashBoard>
+            <Switch>
+              <Route exact path="/" component={DashBoardContend} />
+              <Route exact path="/user" component={UserProfile} />
+            </Switch>
+          </DashBoard>
+        ) : (
+          <Route exact path="/login" component={Login} />
+        )}
+      </div>
     );
   }
 }
